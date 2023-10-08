f00cffc40adace4cd97566aea1bccd5b8f849b74
Oussama-E <oussama.elbouhtain@student.vinci.be>
2023-10-08 11:52:51 +0200

    Tutorials4: fix

--- src/App.jsx
+++ src/App.jsx
@@ -43,12 +43,6 @@ const App = () => {
       content: newNote,
       important: Math.random() < 0.5,
     }
-    axios
-    .post('http://localhost:3001/notes', noteObject)
-    .then(response => {
-      setNotes(notes.concat(response.data))
-      setNewNote('')
-    })
 
   noteService
   .create(noteObject)
